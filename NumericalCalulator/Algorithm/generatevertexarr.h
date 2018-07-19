6efaa505c2d06ca891512de45eb910fd1308cd5d
cjwcommuny@outlook.com <cjwcommuny@outlook.com>
2018-07-19 15:41:22 +0800

    add generateVertexArr

--- NumericalCalulator/Algorithm/generatevertexarr.h
+++ NumericalCalulator/Algorithm/generatevertexarr.h
@@ -1,7 +1,8 @@
 #ifndef GENERATEVERTEXARR_H
 #define GENERATEVERTEXARR_H
+#include "Common/Point"
+#include "Common/Polynomial.h"
 
-
-
+std::vector<Point> generateVertexArr(const LinkList & polynomial);
 
 #endif // GENERATEVERTEXARR_H
